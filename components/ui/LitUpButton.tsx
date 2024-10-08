b54b008b9917c024557866506315963f7a0fe546
lwalker <landen@example.com>
2024-10-08 09:42:06 -0500

    fixed button to look better

--- components/ui/LitUpButton.tsx
+++ components/ui/LitUpButton.tsx
@@ -7,13 +7,12 @@ const LitUpButton = ({
     handleClick?: () => void; otherClasses?: string;
 }) => {
   return (
-    <button className="p-[1px] relative">
-        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-        <div className={`px-8 py-2  bg-transparent rounded-[10px]  relative group transition duration-200 text-white hover:bg-transparent ${otherClasses}`}>
-            {position === 'left' && icon}
+    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-transparent px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}>
             {title}
             {position === 'right' && icon}
-        </div>
+        </span>
     </button>
   )
 }
