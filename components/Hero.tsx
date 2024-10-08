c9aad55aac5c1556806f8fe6725a93056aaf91a9
lwalker <landen@example.com>
2024-10-07 20:00:24 -0500

    hero section background is now gradient colors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,36 +1,35 @@
+import { BackgroundBeams } from "./ui/background-beams"
 import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collision"
-import { LampContainer } from "./ui/lamp"
+import { BackgroundGradientAnimation } from "./ui/background-gradient-animation"
+import { LampDemo } from "./ui/lamp"
+import { TextGenerateEffect } from "./ui/text-generate-effect"
 
 const Hero = () => {
-  return (
-    <div className='pb-20 pt-36'>
+    return (
         <div>
-            <LampContainer className='-top-40 left-10 
-            md:-left-32 md:-top-20 h-screen' fill='white'/>
-            {/* <BackgroundBeamsWithCollision className='-top-40 -left-10 
-            md:-left-32 md:-top-20 h-screen' fill='white'/> */}
-        </div>
-       
-        <div className="h-screen w-full dark:bg-black-100 bg-white  
-        dark:bg-grid-white/[0.03] relative bg-grid-black/[0.2] flex 
-        items-center justify-center" >
-        {/* Radial gradient for the container to give a faded look */}
-            <div className="absolute pointer-events-none 
-            inset-0 flex items-center justify-center dark:bg-black-100 
-            bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-        </div>
-        
-        <div className='flex justify-center relative my-20 z-10'>
-            <div className='max-w-[89vw] md:max-w-2xl 
-            lg:max-w-[60vw] flex flex-col items-center justify-center'>
-                <h2 className='uppercase tracking-widest 
-                text-xs text-center text-blue-100 max-w-80'>
-                    Dynamic Entry by Landen
-                </h2>
+            <div className="fixed inset-0 z-0 w-full h-full bg-gradient-to-b from-blue-500 to-purple-600">
+                {/* <LampDemo className='-top-40 left-10 
+                md:-left-32 md:-top-20 h-screen z-10' /> */}
+                {/* <BackgroundBeamsWithCollision className='-top-40 -left-10 
+                md:-left-32 md:-top-20 h-screen' /> */}
+                {/* <BackgroundBeams className='-top-40 left-10 
+                md:-left-32 md:-top-20 h-screen' /> */}
+                <BackgroundGradientAnimation>
+                    <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+                        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+                        Landen's Portfolio
+                        </p>
+                    </div>
+
+                    
+                </BackgroundGradientAnimation>
             </div>
+           
+            
+            
+            
         </div>
-    </div>
-  )
+      )
 }
 
 export default Hero
