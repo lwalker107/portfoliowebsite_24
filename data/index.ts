1b510402aa408302eb064a020fda27cde4b052d9
lwalker <landen@example.com>
2024-10-08 11:35:21 -0500

    added bento grids but they are at the top of the page instead of below the hero section

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const gridItems = [
+    {title: 'Title1', description: 'Desc1', id: 1},
+    {title: 'Title2', description: 'Desc1', id: 2},
+    {title: 'Title3', description: 'Desc1', id: 3},
+    {title: 'Title4', description: 'Desc1', id: 4},
+]
