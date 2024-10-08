5655c56093e03d84ea1bed1f0f5494fd5e219f2f
lwalker <landen@example.com>
2024-10-07 21:48:40 -0500

    made header mobile responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,15 +12,16 @@ const Hero = () => {
                         </p>
                     
 
-                    <div className='flex justify-center relative my-20 z-10'>
-                        <div className='max-w-[89vw] md:max-w-2xl 
-                        lg:max-w-[60vw] flex flex-col items-center justify-center'>
-                            <TextGenerateEffect 
-                                className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                                words="Transforming Concepts into Seamless Experiences"
-                            />
+                       
+                        <div className='flex justify-center relative my-20 z-10'>
+                            <div className='max-w-full sm:max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                                <TextGenerateEffect 
+                                    className="text-center text-[24px] sm:text-[32px] md:text-5xl lg:text-6xl"
+                                    words="Transforming Concepts into Seamless Experiences"
+                                />
+                            </div>
                         </div>
-                    </div>
+
                     </div>
                 </BackgroundGradientAnimation>
             </div>
