451a03742194f3343f10cefc7b9e02452fb90097
lwalker <landen@example.com>
2024-10-07 21:27:35 -0500

    hero section is laid out how I want it

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,13 +15,22 @@ const Hero = () => {
                 {/* <BackgroundBeams className='-top-40 left-10 
                 md:-left-32 md:-top-20 h-screen' /> */}
                 <BackgroundGradientAnimation>
-                    <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+                    <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
                         <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
                         Landen's Portfolio
                         </p>
-                    </div>
-
                     
+
+                    <div className='flex justify-center relative my-20 z-10'>
+                        <div className='max-w-[89vw] md:max-w-2xl 
+                        lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                            <TextGenerateEffect 
+                                className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                                words="Transforming Concepts into Seamless Experiences"
+                            />
+                        </div>
+                    </div>
+                    </div>
                 </BackgroundGradientAnimation>
             </div>
            
